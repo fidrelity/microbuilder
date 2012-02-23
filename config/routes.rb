45b84fba727975f291c2171efed03fe82b09221a
Andre Schweighofer <nopunktfjutscha@gmail.com>
2012-02-23 13:40:39 +0100

    added user profile

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Microbuilder::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   root :to => 'pages#home'
   get '/editor', :to => 'pages#editor', :as => 'pages_editor'
+  
+  resources :users, :only => [:show]
+
 end
