13475dcf253fdd899e9786f9295e60de5754470e
Andre Schweighofer <nopunktfjutscha@gmail.com>
2012-03-24 18:31:44 +0100

    working delete dependencies

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,12 +2,15 @@ class Game < ActiveRecord::Base
   belongs_to :author, :class_name => 'User', :foreign_key => 'user_id'
   has_and_belongs_to_many :graphics
   
-  after_destroy :destroy_unreferenced_graphics
+  before_destroy :destroy_unreferenced_graphics
   
   private
     def destroy_unreferenced_graphics
       graphics.each do |graphic|
-        graphic.destroy unless graphic.user
+        graphic.games.delete(self)
+        unless graphic.user
+          graphic.destroy
+        end
       end
     end
 end
