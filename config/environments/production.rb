250096de48c7c30df845724e3ee7537a5f7eff91
Andre Schweighofer <nopunktfjutscha@gmail.com>
2012-04-17 18:44:52 +0200

    production syntax fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ PAPERCLIP_OPTIONS = {
   :storage => :s3,
   :bucket => 'mbgfx',
   :s3_credentials => {
-  :access_key_id => ENV['S3_KEY'],
-  :secret_access_key => ENV['S3_SECRET']
+    :access_key_id => ENV['S3_KEY'],
+    :secret_access_key => ENV['S3_SECRET']
+  }
 }
