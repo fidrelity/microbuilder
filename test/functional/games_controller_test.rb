f24dc127169bab993ebdbae67b2267fa9bd6323a
Francois Laptop <webarbeit@gmail.com>
2012-05-15 10:08:38 +0200

    Added test for like and dislike

--- test/functional/games_controller_test.rb
+++ test/functional/games_controller_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class GamesControllerTest < ActionController::TestCase
-  #test "should get home" do
-  #  get :home
-  #  assert_response :success
-  #end
 
   should "increase played by one" do
     @game = Factory.build(:game, :id => 1, :title => "mygame", :user_id => 1, :played => 0)
@@ -12,6 +8,22 @@ class GamesControllerTest < ActionController::TestCase
     put :played, :id => 1
     assert_response 200
     assert_equal 1, @game.reload.played
+  end
+
+  should "increase dislike by one" do
+    @game = Factory.build(:game, :id => 1, :title => "mygame", :user_id => 1, :dislikes => 0)
+    @game.save
+    put :dislike, :id => 1
+    assert_response 200
+    assert_equal 1, @game.reload.dislikes
+  end
+
+  should "increase like by one" do
+    @game = Factory.build(:game, :id => 1, :title => "mygame", :user_id => 1, :likes => 0)
+    @game.save
+    put :like, :id => 1
+    assert_response 200
+    assert_equal 1, @game.reload.likes
   end
 
 end
