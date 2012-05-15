f9622cce617bbd53e7e2cf34245e0cd7a3442530
Francois Laptop <webarbeit@gmail.com>
2012-05-15 21:30:56 +0200

    Clearing comment after submit

--- app/controllers/game_comments_controller.rb
+++ app/controllers/game_comments_controller.rb
@@ -3,13 +3,7 @@ class GameCommentsController < ApplicationController
   
   def create
     @comment = GameComment.new(params[:game_comment])
-    @comment.save    
-
-    #render :json => {:data => @comment, :status => 200}
-    #response, status = [add_comment(@comment), 200]
-    #render :json => @comment, :status => 200
-    
-    #redirect_to play_path(params[:game_comment][:game_id])    
+    @comment.save  
   end
   
   def destroy
