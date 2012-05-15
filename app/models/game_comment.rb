a24af1684d5699449edc03f8872b4595653afba3
Francois Laptop <webarbeit@gmail.com>
2012-05-15 21:59:11 +0200

    Design fix. Added default scope for comments.

--- app/models/game_comment.rb
+++ app/models/game_comment.rb
@@ -3,5 +3,5 @@ class GameComment < ActiveRecord::Base
   belongs_to :user, :class_name => 'User', :foreign_key => 'user_id'
 
   validates_presence_of :comment, :game_id
-  scope :all_latest, order("created_at DESC")
+  default_scope :order => 'created_at DESC'
 end
