9efbc88f77d25868c81fd74d09c46c6bd3372551
Francois Laptop <webarbeit@gmail.com>
2012-05-20 19:50:16 +0200

    Fixed game controller test

--- test/functional/games_controller_test.rb
+++ test/functional/games_controller_test.rb
@@ -5,6 +5,11 @@ class GamesControllerTest < ActionController::TestCase
   should "increase played by one" do
     @game = Factory.build(:game, :id => 1, :title => "mygame", :user_id => 1, :played => 0)
     @game.save
+    
+    #assert_difference(@game.reload.played.to_s, 1) do
+    #  put :played, :id => 1
+    #end
+
     put :played, :id => 1
     assert_equal 1, @game.reload.played
   end
@@ -12,19 +17,20 @@ class GamesControllerTest < ActionController::TestCase
   should "increase dislike by one" do
     @game = Factory.build(:game, :id => 1, :title => "mygame", :user_id => 1, :dislikes => 0)
     @game.save
+    
     xhr :put, :dislike, :id => 1
     assert_response 200
     assert_equal 1, @game.reload.dislikes
   end
 
-  should "increase like by one" do
+  should "increase like by two" do
     @game = Factory.build(:game, :id => 1, :title => "mygame", :user_id => 1, :likes => 0)
     @game.save
-    assert_difference(@game.reload.likes.to_s, 2) do      
-      xhr :put, :like, :id => 1
-      xhr :put, :like, :id => 1
-      #xhr :like, :put => {:id => 1}
-    end    
+
+    xhr :put, :like, :id => 1
+    assert_response 200
+    xhr :put, :like, :id => 1
+    assert_equal 2, @game.reload.likes
   end
 
 end
