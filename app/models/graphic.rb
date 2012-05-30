46741edf746c4f248d1237d379ab6fdcb70b4585
Andre Schweighofer <nopunktfjutscha@gmail.com>
2012-05-30 17:06:46 +0200

    Pushing files to s3 working, using GraphicPreProcessor

--- app/models/graphic.rb
+++ app/models/graphic.rb
@@ -1,6 +1,6 @@
 class Graphic < ActiveRecord::Base
   include PgSearch
-  #include ::GraphicPreProcessor
+  include ::GraphicPreProcessor
 
   attr_accessor :image_data
   attr_accessible :name, :image_data, :image_file_name, :frame_count, :frame_width, :frame_height, :public, :background
@@ -9,7 +9,6 @@ class Graphic < ActiveRecord::Base
   has_and_belongs_to_many :games
 
   before_destroy :referenced?
-  before_create :decode_base64_image
   
   has_attached_file :image, PAPERCLIP_OPTIONS
   
@@ -39,20 +38,6 @@ class Graphic < ActiveRecord::Base
   end
   
   protected
-    def decode_base64_image
-      if image_data
-        content_type = 'image/png'
-        decoded_data = Base64.decode64(image_data.split(/data:image\/png;base64,/).last)
-        
-        data = StringIO.new(decoded_data)
-        data.class.class_eval { attr_accessor :original_filename, :content_type }
-        data.content_type = content_type
-        data.original_filename = File.basename(self.name)
-
-        self.image = data
-      end
-    end
-  
     def self.filter(backgrounds, min = nil, max = nil)
       query = backgrounds ? self.backgrounds : self.without_backgrounds
 
