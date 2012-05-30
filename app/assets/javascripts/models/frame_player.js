faedfb050f9ea33614b103fa5fb3dc9a583fd670
Francois Laptop <webarbeit@gmail.com>
2012-05-30 18:21:55 +0200

    Deleted precomp assets. fixed null object in frame player

--- app/assets/javascripts/models/frame_player.js
+++ app/assets/javascripts/models/frame_player.js
@@ -21,19 +21,18 @@ var FramePlayer = {
     });
   },
 
-  initPlay : function(_object) {    
-    this.totalFrames = parseInt(_object.attr("data-frames"));    
+  initPlay : function(_object) {
+    this.totalFrames = parseInt(_object.attr("data-frames"));
     if(this.totalFrames === 1) return false;
 
     this.currentObject = _object;
     this.frameWidth = _object.width();
     this.currentFrameIndex = 1;
     this.play();
-    this.playInterval = setInterval( function() { FramePlayer.play() ;} , this.frameDuration)
+    this.playInterval = setInterval( function() { FramePlayer.play(); } , this.frameDuration)
   },
 
   play : function() {
-    
     var newPosition = -(this.frameWidth * this.currentFrameIndex);
     this.currentObject.css({"background-position" : newPosition });
 
@@ -47,11 +46,11 @@ var FramePlayer = {
 
   stop : function() {
     clearInterval(this.playInterval);
+    this.playInterval = null;
+    if(this.currentObject) this.currentObject.css({"background-position" : '0px' });
   },
 
-  reset : function() {
-    this.currentObject.css({"background-position" : '0px' });
-
+  reset : function() {    
     this.totalFrames = 0;
     this.currentFrameIndex = 0;
     this.currentObject = null;
