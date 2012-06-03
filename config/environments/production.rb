01a780db2b1193f114d6bef17098129667c4bc4b
Andre Schweighofer <nopunktfjutscha@gmail.com>
2012-06-03 22:23:13 +0200

    heroku deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,8 @@ Playtin::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
+  config.handlebars.precompile = false
+  
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
 
