fe352f59e9093dc5cd9cd8356732234cb23f0c2e
Eberhard Gräther <ebsigraether@hotmail.com>
2012-06-18 05:11:24 +0200

    merged StageView into MainView

--- app/assets/javascripts/editor/views/main_view.js
+++ app/assets/javascripts/editor/views/main_view.js
@@ -19,6 +19,31 @@ var MainView = Ember.View.extend({
     
   },
   
+  didInsertElement : function() {
+    
+    this.$( '#slider' ).slider({
+      
+      min: 5,
+      max: 30,
+      step: 5,
+      value: App.game.duration,
+      
+      slide: function( event, ui ) {
+        
+        App.game.setDuration( ui.value );
+        
+      },
+      
+      change: function( event, ui ) {
+        
+        App.mainView.updatePlayer();
+        
+      }
+      
+    });
+    
+  },
+  
   show : function( name, push ) {
     
     var view = this.get( name );
@@ -68,6 +93,45 @@ var MainView = Ember.View.extend({
       }
     
     });
+    
+  },
+  
+  play : function() {
+    
+    this.player.fsm.start();
+    
+  },
+  
+  stop : function() {
+    
+    this.player.fsm.reset();
+    
+  },
+  
+  trash : function() {
+    
+    if ( confirm( 'Throw the game away?' ) ) {
+      
+      App.gameController.clear();
+      
+      App.gameObjectsController.set( 'current', null );
+      App.behaviourController.set( 'current', null );
+      
+      this.$( '#slider' ).slider( 'value', [5] );
+      
+      if ( window.localStorage ) {
+      
+        window.localStorage.setItem( 'game', null );
+      
+      }
+      
+    }
+    
+  },
+  
+  debug : function() {
+    
+    this.player.debug();
     
   }
   
