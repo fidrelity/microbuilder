ec55838d47a44a3aa6f1ec931f552bd064d98403
Francois Weber <webarbeit@gmail.com>
2012-07-11 19:57:25 +0200

    Stuff before commit

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -1,4 +1,3 @@
-/*
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-32425368-1']);
 _gaq.push(['_trackPageview']);
@@ -8,4 +7,3 @@ _gaq.push(['_trackPageview']);
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
-*/
