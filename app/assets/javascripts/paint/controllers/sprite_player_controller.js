1bb674793d8832310f41230e34c551496584d90a
Francois Laptop <webarbeit@gmail.com>
2012-08-19 12:17:21 +0200

    Fixed sprite player animatin

--- app/assets/javascripts/paint/controllers/sprite_player_controller.js
+++ app/assets/javascripts/paint/controllers/sprite_player_controller.js
@@ -17,41 +17,62 @@ var SpritePlayerController = Ember.Object.extend({
   },
   
   play : function() {
+
     $('#playButton').hide();
     $('#stopButton').show();
+
     $('.active-sprite').removeClass('active-sprite');
+    
     this.playDelay = parseInt( $('#playDelay').val() ) || 200;
+
     this.currentFrameIndex = 0;
+    
     this.overSprites();
+
+    console.log("play", this.playDelay);
+
     this.nextFrame();
   },
 
   nextFrame : function() {
+
     var canvasObjects = this.getSpritesObjects();
+
     canvasObjects.hide();
+    
     canvasObjects.eq(this.currentFrameIndex).show();
 
     if(this.currentFrameIndex == (this.getSpriteCount()) ) {
+
       // Loop
       if( this.isLooping() ) {
+
         this.play();
         return false;
+
       // End
       } else {
+
         this.stop();
         return false;
+
       }
+
     }
 
     this.currentFrameIndex++;
+    
     var that = this;
+    
     this.playInterval = setTimeout(function(){
+
       that.nextFrame();
+
     }, this.playDelay);    
   },
 
   stop : function() {
-
+    
     clearTimeout(this.playInterval);
 
     this.getSpritesObjects().show();
@@ -60,27 +81,36 @@ var SpritePlayerController = Ember.Object.extend({
     $('#playButton').show();
     $('#stopButton').hide();
 
-    App.paintController.setCurrentSpriteModel( App.paintController.getCurrentSpriteModel() );
   },
 
   floatSprites : function() {
+
     this.getSpritesObjects().removeClass('canvas-over').addClass('canvas-float');
+
   },
 
   overSprites : function() {
+
     this.getSpritesObjects().removeClass('canvas-float').addClass('canvas-over');
+
   },
 
   isLooping : function() {
+
     return $("#replayLoop").is(":checked");
+
   },
 
   getSpritesObjects : function() {
-    return $('.canvas').not('#canvas-template, #canvas-sketch, #zoomCanvas');
+
+    return $('.spriteView');
+
   },
 
   getSpriteCount : function() {
+
     return this.getSpritesObjects().length;
+    
   }
 
 });
