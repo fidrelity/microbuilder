29b224458d2f5f1e8fb98e57285ca20b7683a11a
Francois Laptop <webarbeit@gmail.com>
2012-08-28 11:22:39 +0200

    User can see total likes and dislikes on home screen

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
 
   def home
     @games = Game.all_latest.paginate(:page => params[:page], :per_page => 4)
+
+    @likes = current_user.games.sum(:likes)
+    @dislikes = current_user.games.sum(:dislikes)
+
   end
 
   def imprint
