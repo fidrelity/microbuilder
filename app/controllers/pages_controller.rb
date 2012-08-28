a519cd7dab7663c18734b29986c211a82f1cba59
Francois Laptop <webarbeit@gmail.com>
2012-08-28 12:10:32 +0200

    Fixed like stats when user is not logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,10 @@ class PagesController < ApplicationController
   def home
     @games = Game.all_latest.paginate(:page => params[:page], :per_page => 4)
 
-    @likes = current_user.games.sum(:likes)
-    @dislikes = current_user.games.sum(:dislikes)
+    if current_user
+      @likes = current_user.games.sum(:likes)
+      @dislikes = current_user.games.sum(:dislikes)
+    end
 
   end
 
