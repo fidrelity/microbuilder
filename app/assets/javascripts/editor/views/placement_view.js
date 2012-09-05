c8573f21733300d595a5463039448edfdd08d266
Francois Laptop <webarbeit@gmail.com>
2012-09-05 11:45:54 +0200

    Removed textfield for angle in direction trigger

--- app/assets/javascripts/editor/views/placement_view.js
+++ app/assets/javascripts/editor/views/placement_view.js
@@ -28,6 +28,8 @@ var PlacementView = Ember.View.extend({
   
   increment : { x : 96, y : 60 },
   scale : 2,
+
+  directionAngle : 0,
   
   didInsertElement : function() {
     
@@ -39,6 +41,8 @@ var PlacementView = Ember.View.extend({
 
 
     this.addObserver( 'directionAngle', function() {
+      
+      console.log(self.observer)
       
       self.set("directionAngle", 90);
       
@@ -181,7 +185,7 @@ var PlacementView = Ember.View.extend({
       
       if ( type === 'direction' ) {
       
-        this.object.pos.addSelf( obs.location );
+        this.object.pos.addSelf( obs.location );       
 
       } else if ( type === 'offset' ) {
         
@@ -330,6 +334,7 @@ var PlacementView = Ember.View.extend({
 
         var angle = new Vector( -this.width * 0.5, -this.height * 0.5 ).addSelf( this.object.pos ).angle();
         this.set("directionAngle", Math.round( angle / Math.PI * 180) );
+        console.log(Math.round( angle / Math.PI * 180) );
         
       } else if ( this.type === 'path' ) {
         
@@ -472,9 +477,16 @@ var PlacementView = Ember.View.extend({
   
   isDirection : function() {
     
-    return this.type === "direction";
+    //return this.type === "direction";
+    return false;
     
   }.property("direction"),
+
+  // getDirectionAngle : function() {
+
+  //   return 20;
+
+  // }.observes("directionAngle"),
 
   clearPath : function() {
     
