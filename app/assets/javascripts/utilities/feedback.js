9ea145beea6aa8dc0b9e821c8e6e54da0bbee192
Francois Laptop <webarbeit@gmail.com>
2012-09-08 10:30:58 +0200

    Feedback form now with error message and user link in body if user sigend in

--- app/assets/javascripts/utilities/feedback.js
+++ app/assets/javascripts/utilities/feedback.js
@@ -1,36 +1,76 @@
 var Feedback = {
+
   wrapper : null,
+
   textarea : null,
+
   subject : null,
 
   init : function() {
+
     Feedback.wrapper = $("#feedback");
+
     $('.showFeedback').click(function() { Feedback.show(); });
     $('.closeFeedback').click(function() { Feedback.close(); });
+
     Feedback.textarea = Feedback.wrapper.find("#body");
     Feedback.subject = Feedback.wrapper.find("#subject");
+    Feedback.errorWrapper = Feedback.wrapper.find("#feedbackErrorMessage");
+
   },
 
   show : function() {
+
     Feedback.clear();
+
     if(Feedback.wrapper.is(":visible")) {
+
       Feedback.close();
+
     } else {
+
       var newX = ($(document).width() / 2) - (Feedback.wrapper.width() / 2);
       var newY = 200;
       Feedback.wrapper.css({left: newX, top: newY}).fadeIn(800);
       Feedback.subject.focus();
+
     }
+
   },
 
   clear : function() {
+
     Feedback.subject.val("");
     Feedback.textarea.val("");
     Feedback.textarea.removeClass("errorForm");
+
+  },
+
+  afterSent : function(state) {
+
+    if(state === "success") {
+
+      Feedback.close();
+      Notifier.add('Support message has been sent! Thank you!', "success").notify();
+
+    } else {
+
+      Feedback.showError();
+
+    }
+
+  },
+
+  showError : function() {
+
+    Feedback.errorWrapper.show();
+
   },
 
   close : function(_time) {
+
     var _time = _time || 0;
-    Feedback.wrapper.hide(_time);    
+    Feedback.wrapper.hide(_time);
+
   }
 };
--- app/controllers/support_controller.rb
+++ app/controllers/support_controller.rb
@@ -5,13 +5,17 @@ class SupportController < ApplicationController
   respond_to :js, :only => [:feedback, :ticket, :report]
 
   def feedback
-    create_issue "Feedback: #{params[:subject]}",  params[:body], "Feedback"
+    issue_state = create_issue "Feedback: #{params[:subject]}", params[:body], "Feedback"
+    @message_type = issue_state ? "success" : "error"
   end
 
   def report
     game_link = root_url + "play/#{params[:gid]}"
-    body = params[:body] + "\n\n#{game_link}"
-    create_issue "Report: #{params[:type]} [#{params[:gid]}]", body, "Report"
+    
+    body = params[:body] + "\n\n#{game_link}" if params[:body]
+
+    issue_state = create_issue "Report: #{params[:type]} [#{params[:gid]}]", body, "Report"
+    @message_type = issue_state ? "success" : "error"
   end
   
   def ticket
@@ -24,7 +28,19 @@ class SupportController < ApplicationController
 
   def create_issue title, body, label = "question"
     return false if title.empty? || body.empty?
-    Feedhub::open_issue(:title => title, :body => body, :label => label)
+    Feedhub::open_issue(:title => title, :body => append_user_to_body( body ), :label => label)
+    true
+  end
+
+  def append_user_to_body(body)
+
+    if user_signed_in?
+      user_link = root_url + "users/#{current_user.id}"
+      body = body + "\n\nFrom: #{current_user.display_name}\n#{user_link}" if !body.empty?
+    end
+
+    return body
+
   end
 
 end
