388cfdbb964b4ad3716bbccd2693f9ce029e92cd
Francois Laptop <webarbeit@gmail.com>
2012-10-01 21:51:56 +0200

    Fixed: image is hidding after deleteing it. Issue #208

--- app/controllers/graphics_controller.rb
+++ app/controllers/graphics_controller.rb
@@ -20,13 +20,13 @@ class GraphicsController < ApplicationController
   
   def destroy
     @user = current_user
-    graphic = @user.graphics.find(params[:id])
-    flash[:success] = "Successfully deleted graphic" if graphic && graphic.destroy
+    @graphic = @user.graphics.find(params[:id])
+    flash[:success] = "Successfully deleted graphic" if @graphic && @graphic.destroy
     @graphics = @user.graphics.without_backgrounds.with_authorization(current_user).paginate(:page => params[:graphics], :per_page => 12)
     @backgrounds = @user.graphics.backgrounds.with_authorization(current_user).paginate(:page => params[:backgrounds], :per_page => 12)
     @games = @user.games.paginate(:page => params[:games], :per_page => 9)
 
-    @was_background = graphic.background?
+    @was_background = @graphic.background?
   end
 
   def publish
