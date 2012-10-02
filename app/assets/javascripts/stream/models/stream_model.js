7fa6a3ded20217cb22c2429f9bd0626b40656383
Francois Laptop <webarbeit@gmail.com>
2012-10-02 19:52:01 +0200

    Files for realtime stream

---
+++ app/assets/javascripts/stream/models/stream_model.js
@@ -0,0 +1,13 @@
+var StreamModel = Ember.Object.extend({
+  
+  
+  
+});
+
+// -------------------------------------------
+
+var GameActivityModel = StreamModel.extend({
+  
+  templateName: 'stream/templates/create_game_template',
+
+});
