1b2e4ac01c8a1f3a88153fc3e28671c0395c1002
Francois Laptop <webarbeit@gmail.com>
2012-11-03 13:01:32 +0100

    Removed facebook publish_stream permission

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -211,7 +211,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :facebook, FACEBOOK_ID, FACEBOOK_SECRET, :scope => 'email,publish_stream', :client_options => {:ssl => {:ca_path => '/etc/ssl/certs'}}
+  # auto-share-permission: publish_stream
+  config.omniauth :facebook, FACEBOOK_ID, FACEBOOK_SECRET, :scope => 'email', :client_options => {:ssl => {:ca_path => '/etc/ssl/certs'}}
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
