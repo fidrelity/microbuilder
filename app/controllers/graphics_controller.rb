b4d6b8eb93137a08d1c927dc07010f09c9281bc3
Francois <webarbeit@gmail.com>
2013-02-16 13:31:49 +0100

    Before filter to get graphic id

--- app/controllers/graphics_controller.rb
+++ app/controllers/graphics_controller.rb
@@ -1,13 +1,13 @@
 class GraphicsController < ApplicationController
   respond_to :js, :only => [:create, :show, :public, :destroy, :publish]
   before_filter :authenticate_user!, :only => [:create, :destroy]
+  before_filter :get_id, :only => [:show, :destroy, :publish, :games, :report]
   
   def index
     @graphics = Graphic.all
   end
 
   def show
-    @graphic = Graphic.find(params[:id])
     response = @graphic.to_response_hash(current_user) if (@graphic.user == current_user || @graphic.public)
     render :json => response
   end
@@ -20,7 +20,6 @@ class GraphicsController < ApplicationController
   
   def destroy
     @user = current_user
-    @graphic = @user.graphics.find(params[:id])
     flash[:success] = "Successfully deleted graphic" if @graphic && @graphic.destroy
     @graphics = @user.graphics.without_backgrounds.with_authorization(current_user).paginate(:page => params[:graphics], :per_page => 12)
     @backgrounds = @user.graphics.backgrounds.with_authorization(current_user).paginate(:page => params[:backgrounds], :per_page => 12)
@@ -30,8 +29,6 @@ class GraphicsController < ApplicationController
   end
 
   def publish
-    @graphic = Graphic.find(params[:id])
-
     if @graphic.user == current_user      
       if @graphic.update_attribute(:public, true)
         Stream.create_message("graphic_publish", @graphic.user, @graphic)
@@ -41,8 +38,7 @@ class GraphicsController < ApplicationController
   end
 
   def games
-    graphic = Graphic.find(params[:id])
-    @games = graphic.games.all_latest("desc").paginate(:page => params[:page], :per_page => 12)    
+    @games = @graphic.games.all_latest("desc").paginate(:page => params[:page], :per_page => 12)    
   end
   
   def public
@@ -88,6 +84,10 @@ class GraphicsController < ApplicationController
     render :text => img_data
   end
 
+  def report
+
+  end
+
   protected
 
     # Retuns all graphics with name like %term%
@@ -95,5 +95,9 @@ class GraphicsController < ApplicationController
       isBackground = isBackground === "undefined" ? false : isBackground
       graphics = Graphic.order(:name).where("lower(name) like ? AND background = ?", "%#{term.downcase}%", isBackground)
     end      
+
+    def get_id
+      @graphic = Graphic.find(params[:id])
+    end
 
 end
