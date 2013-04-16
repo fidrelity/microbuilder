bd9e082a8d1cb1363f30b66b63d33c385b2253bb
Andre Schweighofer <nopunktfjutscha@gmail.com>
2013-04-16 15:18:02 +0200

    second try fixing sendgrid

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
-  :authentication => :plain,
+  :authentication => :login,
   :user_name      => ENV['SENDGRID_USERNAME'],
   :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com'
