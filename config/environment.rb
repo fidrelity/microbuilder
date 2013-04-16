c4daf4692c362c22bca4c31fabbbd8d6a2a1c77b
Andre Schweighofer <nopunktfjutscha@gmail.com>
2013-04-16 14:33:13 +0200

    trying to fix mailing on production

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Playtin::Application.initialize!
 Date::DATE_FORMATS.merge!(:default => "%d.%m.%Y")
+
+ActionMailer::Base.delivery_method = :smtp
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -6,4 +6,3 @@ ActionMailer::Base.smtp_settings = {
   :password       => ENV['SENDGRID_PASSWORD'],
   :domain         => 'heroku.com'
 }
-ActionMailer::Base.delivery_method = :smtp
